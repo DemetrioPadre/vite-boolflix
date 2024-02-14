3771e7beec28664027e066007532ba053cc21a1b
DemetrioPadre <demetriopadre@gmail.com>
2024-02-14 17:10:13 +0100

    add appheader only file

---
+++ src/components/App.vue
@@ -0,0 +1,15 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+    components: { AppHeader }
+};
+</script>
+<template>
+    <AppHeader>ciao</AppHeader>
+</template>
+<style lang ="scss">
+@use'./styles/general.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+import { store } from '../store/index';
+
+export default {}
+
+</script>
+
+<template></template>
+
+
+<style lang="scss"></style>
+
+
