3563df2026b8cb37c17b2e8321833544d289ee91
DemetrioPadre <demetriopadre@gmail.com>
2024-02-15 22:58:38 +0100

    add app main

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,6 @@ export const store = reactive({
     apiUriMovie: 'https://api.themoviedb.org/3/search/movie?api_key=6b67ac9052f72f32a1135373465f12ea&query=',
     apiUriTv: ' https://api.themoviedb.org/3/search/tv?api_key=6b67ac9052f72f32a1135373465f12ea&query=',
     apiKey: '6b67ac9052f72f32a1135373465f12ea',
-    filmList: [],
+    moviesList: [],
     tvSeries: [],
 });
