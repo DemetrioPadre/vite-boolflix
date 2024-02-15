1db85d201045bc43a405dc4a6c5227c095128dd1
DemetrioPadre <demetriopadre@gmail.com>
2024-02-15 22:24:22 +0100

    add serie tv

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,15 @@ export default {
 
   methods: {
     pippoSearch() {
-      axios.get(store.apiUri + this.searchGeneral).then((response) => {
+      axios.get(store.apiUriMovie + this.searchGeneral).then((response) => {
         console.log(response.data.results);
-        store.movies = response.data.results;
+        store.filmList = response.data.results;
+      });
+    },
+    plutoSearch() {
+      axios.get(store.apiUriTv + this.searchGeneral).then((response) => {
+        console.log(response.data.results);
+        store.tvSeries = response.data.results;
       });
     }
   }
@@ -25,15 +31,25 @@ export default {
 
 <template>
   <div class="container mt-3">
-    <input type="text" class="form-control" v-model="searchGeneral" @keyup.enter="pippoSearch()">
-    <button class="btn btn-primary " @click="pippoSearch()">schiacciami</button>
+    <input type="text" class="form-control" v-model="searchGeneral" @keyup.enter="pippoSearch(), plutoSearch()"
+      placeholder="come ti posso aiutare">
+    <button class="btn btn-primary " @click="pippoSearch(), plutoSearch()">schiacciami</button>
   </div>
   <div>
-    <ul v-for="movie in store.movies">
+    <ul v-for="movie in store.filmList">
       <li>titolo: {{ movie.title }}</li>
       <li>titolo originale: {{ movie.original_title }}</li>
       <li> lingua: {{ movie.original_language }}</li>
-      <li> voto: {{ movie.vote_average }}</li>
+      <li> voto: {{ movie.vote_average / 2 }}</li>
+    </ul>
+  </div>
+
+  <div>
+    <ul v-for="serie in store.tvSeries">
+      <li>titolo: {{ serie.title }}</li>
+      <li>titolo originale: {{ serie.original_title }}</li>
+      <li> lingua: {{ serie.original_language }}</li>
+      <li> voto: {{ serie.vote_average / 2 }}</li>
     </ul>
   </div>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { reactive } from 'vue';
 export const store = reactive({
-    apiUri: 'https://api.themoviedb.org/3/search/movie?api_key=6b67ac9052f72f32a1135373465f12ea&query=',
+    apiUriMovie: 'https://api.themoviedb.org/3/search/movie?api_key=6b67ac9052f72f32a1135373465f12ea&query=',
+    apiUriTv: ' https://api.themoviedb.org/3/search/tv?api_key=6b67ac9052f72f32a1135373465f12ea&query=',
     apiKey: '6b67ac9052f72f32a1135373465f12ea',
+    filmList: [],
+    tvSeries: [],
 });
